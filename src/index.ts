3e7d5e53a189b7aa803a9e9a36e8799350c7ed6b
Derrick Hammer <derrick@derrickhammer.com>
2023-03-27 08:12:53 -0400

    *Update module hash

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ import { RpcQueryOptions } from "@lumeweb/rpc-client";
 import { Buffer } from "buffer";
 import { Client, factory } from "@lumeweb/libkernel-universal";
 
-const RPC_MODULE = "fAAgZfXMqN3YOn0-b9DICt8OPsOFeWw3YKY2p84aytzBww";
+const RPC_MODULE = "fAAcoX42upsbtM8I-9VVK8pA7OzLDWYcExhUtN3Q2Mtr7A";
 
 export class RpcNetwork extends Client {
   private _def: boolean;
